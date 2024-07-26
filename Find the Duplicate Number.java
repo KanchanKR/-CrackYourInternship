0aa0aa7a113a85f7a4b285b67333d3125bb5171c
KanchanKR <renapurkarkanchan@gmail.com>
2024-07-26 14:22:20 +0530

    day one questions

---
+++ Find the Duplicate Number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findDuplicate(int[] arr) {
+
+        int slow = arr[0];
+    int fast = arr[0];
+    do
+    {
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    } while (slow != fast);
+
+    // Find the "entrance"
+    // to the cycle.
+    int ptr1 = arr[0];
+    int ptr2 = slow;
+    while (ptr1 != ptr2) 
+    {
+        ptr1 = arr[ptr1];
+        ptr2 = arr[ptr2];
+    }
+
+    return ptr1;
+    }
+}
